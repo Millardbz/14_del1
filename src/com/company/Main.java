0fa609f2866541f81fd8f3458cd93bbc8bca0e8c
Mathilde Elia <mathildeselia@gmail.com>
2021-09-28 12:39:57 +0200

    første udkast af point system

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int dice1=(int)(Math.random()*6+1);
-        int dice2=(int)(Math.random()*6+1);
+        int p1 = 0, p2 = 0;
 
-        System.out.println(dice1);
-        System.out.println(dice2);
+        while(p1<40) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Press enter to roll your dice");
+
+            String roll = scan.nextLine();
+            if (roll == roll) {
+                int dice1 = (int) (Math.random() * 6 + 1);
+                int dice2 = (int) (Math.random() * 6 + 1);
+
+                int sum1 = (dice1 + dice2);
+                int sum2 = (dice1 + dice2);
+
+                p1 += sum1;
+                p2 += sum2;
+
+                System.out.println("You rolled a " + dice1 + " and " + dice2);
+                System.out.println("You now have " + p1 + " points");
+            }
+        }
     }
 }
