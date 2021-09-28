9d4bb7efd3d02bfd3311e29b92e687a891c2525c
Elias <s215817@student.dtu.dk>
2021-09-28 12:26:45 +0200

    det er to 1ere

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int dice1=(int)(Math.random()*6+1);
-        int dice2=(int)(Math.random()*6+1);
 
-        System.out.println(dice1);
-        System.out.println(dice2);
+
+        while (true) {
+            int dice1 = (int) (Math.random() * 6 + 1);
+            int dice2 = (int) (Math.random() * 6 + 1);
+            int sum = dice1 + dice2;
+
+
+            System.out.println("Roll: total = " + sum);
+
+            if (sum == 2) {
+                System.out.println(new StringBuilder().append("Sorry with a ").append(sum).append(" You lose all your points:(").toString());
+                break;
+            } else if (sum >= 3) {
+                System.out.println("you get  " + sum + " points" );
+                break;
+            }
+
+
+        }
     }
 }
