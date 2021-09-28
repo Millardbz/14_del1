4a82843d55d53cc60f645bdd4db6e78c6f4005ef
Malte <maltebjoerklund@gmail.com>
2021-09-28 16:06:03 +0200

    Loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 	// write your code here
-        int dice1=(int)(Math.random()*6+1);
-        int dice2=(int)(Math.random()*6+1);
+
+        int dice1, dice2;
+
+    do{
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Roll (enter)");
+        scan.nextLine();
+
+        dice1 = (int)(Math.random()*6+1);
+        dice2 = (int)(Math.random()*6+1);
 
         System.out.println(dice1);
         System.out.println(dice2);
+
+
+        }while(dice1 != dice2);
+
     }
 }
