3e52aae83bab132b5c095e8a1e13360940f2153c
Mathilde Elia <mathildeselia@gmail.com>
2021-09-29 14:27:45 +0200

    Har tilføjet skift tur og prøvet på to ens.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -7,24 +8,62 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int p1 = 0, p2 = 0;
+        int rolls = 1;
+        int die1 =1, die2 = 2;
 
-        while(p1<40) {
+        while(rolls<10) {
+
+            if (die2 != die1) {
+
+                if (rolls % 2 == 0){
+                System.out.println("Player 2's turn!");}
+                else {
+                System.out.println("Player 1's turn");}
+
+            rolls++;
             Scanner scan = new Scanner(System.in);
             System.out.println("Press enter to roll your dice");
-
             String roll = scan.nextLine();
-            if (roll == roll) {
-                int dice1 = (int) (Math.random() * 6 + 1);
-                int dice2 = (int) (Math.random() * 6 + 1);
 
-                int sum1 = (dice1 + dice2);
-                int sum2 = (dice1 + dice2);
+            die1 = (int) (Math.random() * 6 + 1);
+            die2 = (int) (Math.random() * 6 + 1);
 
-                p1 += sum1;
-                p2 += sum2;
+            int sum = (die1 + die2);
 
-                System.out.println("You rolled a " + dice1 + " and " + dice2);
-                System.out.println("You now have " + p1 + " points");
+                if (rolls % 2 == 0){
+                p2 += sum;}
+                else {
+                p1 += sum;}
+
+                System.out.println("You rolled a " + die1 + " and " + die2);
+
+                if (rolls % 2 == 0){
+                System.out.println("You now have " + p2 + " points");}
+                else{
+                System.out.println("You now have " + p1 + " points");}
+            }
+            else {
+                System.out.println("And you got a pair! Press enter to roll your dice again");
+                Scanner scan = new Scanner(System.in);
+                String roll = scan.nextLine();
+
+                die1 = (int) (Math.random() * 6 + 1);
+                die2 = (int) (Math.random() * 6 + 1);
+
+                int sum = (die1 + die2);
+
+                if (rolls % 2 == 0){
+                    p2 += sum;}
+                else {
+                    p1 += sum;}
+
+                System.out.println("You rolled a " + die1 + " and " + die2);
+
+                if (rolls % 2 == 0){
+                    System.out.println("You now have " + p2 + " points");}
+                else{
+                    System.out.println("You now have " + p1 + " points");}
+
             }
         }
     }
