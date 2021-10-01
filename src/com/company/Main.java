948774cd83378499bdca65e73cd053e66c94ea68
45222 <s125781@dtu.dk>
2021-10-01 11:22:57 +0200

    Space

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,9 @@ public class Main {
         int dice1=(int)(Math.random()*6+1);
         int dice2=(int)(Math.random()*6+1);
 
+
+
+
         System.out.println(dice1);
         System.out.println(dice2);
     }
